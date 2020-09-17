b6a460a42204168b3ca65b95a8402590e92e1460
VictorZ94 <zrvictor@outlook.com>
2020-09-17 02:18:30 +0000

    doble linked list at the stack

---
+++ linked_list.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+ * add_dnodeint - add node at beginning of the nodes.
+ * @head: Pointer at top of the nodes
+ * @n: Number that contains the node
+ * Return: Pointer to a new node.
+ */
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *NewNode;
+
+	NewNode = malloc(sizeof(stack_t));
+	if (!NewNode)
+		return (NULL);
+
+	NewNode->n = n;
+	if ((*head) != NULL)
+		(*head)->prev = NewNode;
+	NewNode->next = *head;
+	NewNode->prev = NULL;
+	*head = NewNode;
+
+	return (NewNode);
+}
+
+/**
+ * free_dlistint - freed memory allocation.
+ * @head: Pointer at top of the nodes
+ *
+ * Return: void
+ */
+void free_dlistint(stack_t *head)
+{
+	if (head == NULL)
+		return;
+
+	free_dlistint(head->next);
+	free(head);
+}
